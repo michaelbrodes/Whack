8fd003f29454b6feb38e19fab354769321cd1315
Michael Rhodes <michaelleerhodes@gmail.com>
2016-11-11 22:33:20 -0600

    Created registerPress on frontend
    registerPress will check the current keypress is equal the first
    element in the input string, then it color code the first character
    depending on that equality.

--- assets/js/whack.js
+++ assets/js/whack.js
@@ -15,6 +15,43 @@
         return TEMPLATE_DIR + htmlFile + ".html";
     }
 
+    /**
+     * Jquery callback for registering a keypress on document
+     * @param e - the jquery event object for keypress
+     * @param $scope - the angular scope object
+     */
+    function registerPress( e, $scope ) {
+        // shorthand names
+        var phrase = $scope.phrase;
+        var suc = $scope.success;
+        var fail = $scope.failure;
+        var inputChar = String.fromCharCode(e.which);
+
+        if ( phrase[0] === inputChar && fail.length === 0 )
+        {
+            // add the beginning of phrase to success' end.
+            suc = suc.concat(phrase[0]);
+            phrase = phrase.substr(1, phrase.length);
+            fail = "";
+        }
+        else if ( inputChar === fail[0] )
+        {
+            // pop the failed character into the success string
+            suc = suc.concat(fail[0]);
+            fail = "";
+        }
+        else if ( fail.length === 0 )
+        {
+            // take the end of phrase and append to fail
+            fail = fail.concat(phrase[0]);
+            phrase = phrase.substr(1, phrase.length);
+        }
+
+        $scope.phrase = phrase;
+        $scope.success = suc;
+        $scope.failure = fail;
+    }
+
     whack.config(['$routeProvider', '$locationProvider',
         function ( $routeProvider, $locationProvider ) {
             $routeProvider
@@ -35,8 +72,6 @@
                     controller: "mainController"
                 })
             ;
-            // remove the hash from the route
-            $locationProvider.html5Mode(true);
 
     }]);
 
@@ -45,9 +80,29 @@
 
     }]);
 
-    whack.controller('gameController', ['$http', '$scope',
-        function( $http, $scope ){
+    whack.controller('gameController', ['$http', '$scope', '$location', function(
+        $http, $scope, $location
+    ){
+        // initial DOM values
+        $scope.success = "";
+        $scope.failure = "";
+        $scope.phrase = "Hello World";
 
+        var $doc = angular.element(document);
+        var lengthToWin = $scope.phrase.length;
+
+        $doc.keypress(function ( e ) {
+            $scope.$apply(function() {
+                registerPress(e, $scope);
+            });
+
+            if (lengthToWin === $scope.success.length)
+            {
+                $scope.$apply(function () {
+                    $location.path("/leaderboard");
+                });
+            }
+        });
     }]);
 
     whack.controller('leadController', ['$http', '$scope',
